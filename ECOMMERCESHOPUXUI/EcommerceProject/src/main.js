6f29c52b081803ff507e039c5a29a094d414f7e5
Trực Trần Trung <tructtpk03625@gmail.com>
2025-05-30 21:23:27 +0700

    refactor(main): use alias for autoReplaceImages import and initialize API base URL

--- ECOMMERCESHOPUXUI/EcommerceProject/src/main.js
+++ ECOMMERCESHOPUXUI/EcommerceProject/src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import replaceBrokenImages from './utils/autoReplaceImages'
+import replaceBrokenImages from '@/utils/autoReplaceImages'
+import { initApiBaseUrl } from '@/utils/axiosClient'
 
 import App from './App.vue'
 import router from './router'
 const app = createApp(App)
+
+await initApiBaseUrl()
 
 app.use(createPinia())
 app.use(router)
