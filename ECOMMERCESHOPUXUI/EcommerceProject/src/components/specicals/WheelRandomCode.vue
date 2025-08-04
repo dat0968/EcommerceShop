eddcf52039f1ad4b8d2fd4ba1c047c80504cdd93
Trực Trần Trung <tructtpk03625@gmail.com>
2025-08-04 21:43:26 +0700

    fix(wheel): remove unnecessary checkSpinCount call before fetching prize list

--- ECOMMERCESHOPUXUI/EcommerceProject/src/components/specicals/WheelRandomCode.vue
+++ ECOMMERCESHOPUXUI/EcommerceProject/src/components/specicals/WheelRandomCode.vue
@@ -351,7 +351,6 @@ export default {
         }
       }
 
-      await this.checkSpinCount();
       this.fetchPrizeList();
 
       const wheelSwalDate = localStorage.getItem('wheel_swal_date') || '';
