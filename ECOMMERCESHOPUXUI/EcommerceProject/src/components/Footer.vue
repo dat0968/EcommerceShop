a43d3ae192b15ad4036dbb2995c209d7916b2752
Trực Trần Trung <tructtpk03625@gmail.com>
2025-08-12 23:37:01 +0700

    fix: update import paths for NavigationUserReview and WheelRandomCode components

--- ECOMMERCESHOPUXUI/EcommerceProject/src/components/Footer.vue
+++ ECOMMERCESHOPUXUI/EcommerceProject/src/components/Footer.vue
@@ -106,7 +106,7 @@
               <h6>Tài khoản</h6>
               <ul>
                 <li><RouterLink to="/Profile">Tài khoản của tôi</RouterLink></li>
-                <li><RouterLink to="/FollowOrder">Theo dõi đơn hàng</RouterLink></li>
+                <li><RouterLink to="/order">Theo dõi đơn hàng</RouterLink></li>
                 <li><RouterLink to="/cart">Thanh toán</RouterLink></li>
                 <li><RouterLink to="/favoriteproduct">Danh sách yêu thích</RouterLink></li>
               </ul>
